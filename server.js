cad9798ff3500738c4cf4f3e58dd674c12041f85
Karthick M <mkarthickakmn@gmail.com>
2020-08-01 11:45:43 +0530

    ang init commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/sample01'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/sample01/index.html'));
+});
+
+// default Heroku PORT
+app.listen((process.env.PORT || 3000),()=>{
+	console.log("connected");
+});
